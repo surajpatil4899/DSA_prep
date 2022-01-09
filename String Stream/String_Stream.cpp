35e63b8637dc8f5af32b586433eadaf4cc48dab1
surajpatil4899 <85224768+surajpatil4899@users.noreply.github.com>
2022-01-09 14:19:03 +0530

    Add files via upload

---
+++ String Stream/String_Stream.cpp
@@ -0,0 +1,34 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	// Complete this function
+    vector<int> v;
+    stringstream ss(str);
+    int n;
+    char skip;
+    while(ss){
+        if(ss.peek() != ','){
+            if(ss>>n){
+                v.push_back(n);
+            }
+        }
+        else{
+            ss>>skip;
+        }
+    }
+    return v;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
