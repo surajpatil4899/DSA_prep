a19ae63ddc9b3ebb47a669f3b1582c93fc3e666a
surajpatil4899 <85224768+surajpatil4899@users.noreply.github.com>
2022-01-18 15:13:41 +0530

    Add files via upload

---
+++ Two Sum/Two_Sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+         unordered_map<int,int> indices;
+        for(int i=0;i<nums.size();i++){
+         if(indices.find(target-nums[i]) != indices.end()) {
+           ans.push_back(indices[target-nums[i]]);
+            ans.push_back(i);
+             return ans;
+         }
+          indices[nums[i]]=i;
+        }
+        
+            return ans;
+    }
+    
+
+};
