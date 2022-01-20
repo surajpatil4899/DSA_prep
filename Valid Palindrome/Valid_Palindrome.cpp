e8bd104f9670288606438e1e6c384fd8c7cca419
surajpatil4899 <85224768+surajpatil4899@users.noreply.github.com>
2022-01-20 13:31:09 +0530

    Add files via upload

---
+++ Valid Palindrome/Valid_Palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+      if(s.size() <= 1) return true;
+        int start = 0;
+        int end = s.size()-1;
+        while(start<end){
+            while(start<end && !isalnum(s[start])) start++;
+            while(start<end && !isalnum(s[end])) end--;
+            if(start<end && tolower(s[start]) != tolower(s[end])) return false;
+            start++;
+            end--;
+        }
+        
+        return true;
+    }
+};
