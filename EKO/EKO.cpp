1cb468d0aafac71b71f16b1a3c702c135a955636
surajpatil4899 <85224768+surajpatil4899@users.noreply.github.com>
2022-01-25 18:35:30 +0530

    Add files via upload

---
+++ EKO/EKO.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e6+10;
+int n;
+long long m;
+long long trees[N];
+
+bool isWoodSufficient(int h){
+     int sum=0;
+     for(int i=0;i<n;i++){
+     	if(trees[i] >= h)
+     	sum += trees[i] - h;
+     }
+
+     return sum>=m;
+}
+
+int main(){
+	cin>>n>>m;
+	for(int i=0;i<n;i++){
+		cin>>trees[i];
+	}
+
+	long long lo=0, hi=1e9, mid;
+
+	while(hi-lo >1){
+		mid = (hi+lo)/2;
+		if(isWoodSufficient(mid)){
+			lo = mid;
+		}
+		else hi = mid-1;
+	}
+	if(isWoodSufficient(hi))
+		cout<<hi<<endl;
+	else cout<<lo<<endl;
+}
