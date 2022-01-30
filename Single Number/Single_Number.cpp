83b2ef17492346271e73d9c280c53f210eb147ee
surajpatil4899 <85224768+surajpatil4899@users.noreply.github.com>
2022-01-30 19:40:12 +0530

    Add files via upload

---
+++ Single Number/Single_Number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int,int> hash;
+  
+        for(int i=0;i<nums.size();i++){
+            hash[nums[i]]++;
+        }
+        for(int i=0;i<nums.size();i++){
+           if(hash[nums[i]]==1)
+              return nums[i]; 
+        }
+        
+        return -1;
+    }
+};
