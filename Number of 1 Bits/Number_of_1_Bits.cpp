d1c84be6b965216a8d6e9f05c5f1e8a8e2f731da
surajpatil4899 <85224768+surajpatil4899@users.noreply.github.com>
2022-01-31 01:10:57 +0530

    Add files via upload

---
+++ Number of 1 Bits/Number_of_1_Bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        for(int i=31;i>=0;i--){
+           if((n&(1<<i)) != 0) count++;
+        }
+        
+        return count;
+    }
+};
