b4a24bb022014eef1c5b4b0769ae795208225aa1
surajpatil4899 <85224768+surajpatil4899@users.noreply.github.com>
2022-02-01 19:11:21 +0530

    Add files via upload

---
+++ Minimum Moves to Reach Target Score/Minimum-moves-to-reach-target-score.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minMoves(int target, int maxDoubles) {
+       if(maxDoubles==0) return target-1;
+        int count = 0;
+        while(maxDoubles>0){
+          if(target==1) break;
+            if(target%2==0){
+                target /=2;
+                maxDoubles--;
+                count++;
+            }
+            else{
+                target--;
+                count++;
+            }
+        }
+        
+        if(target>0) return count +(target-1);
+        
+        return count;
+    }
+};
