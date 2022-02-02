126dc0a7c77bdf62b0bdc26a9c664dc7b7d8cbdd
surajpatil4899 <85224768+surajpatil4899@users.noreply.github.com>
2022-02-02 15:31:20 +0530

    Add files via upload

---
+++ Unlock The Door/Unlock_the_Door.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int M = 1e9+7;
+const int N = 1e5+10;
+int fact[N];
+
+int binaryExp(int a,int b, int m){
+	int ans = 1;
+	while(b){
+		if(b&1){
+			ans = (ans * 1LL *a)%m;
+		}
+		a = (a *1LL *a)%m;
+		b >>= 1;
+	}
+
+	return ans;
+}
+
+int main(){
+   int t;
+   cin>>t;
+   fact[0] = 1;
+   for(int i=1;i<N;i++){
+   	fact[i] = (fact[i-1] *1LL *i)%M;
+   }
+   while(t--){
+   	int n,k;
+   	cin>>n>>k;
+   	int ans = fact[n];
+   	ans = (ans*1LL*fact[k])%M;
+   	int deno = (fact[k-n] *1LL *fact[n])%M;
+   	ans = (ans * 1LL * binaryExp(deno,M-2,M))%M;
+      cout<<ans<<endl;
+   }
+}
