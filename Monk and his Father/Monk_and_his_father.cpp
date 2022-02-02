9d8151f840a5d8f7e4f332aa71e3f67c6cbf89d4
surajpatil4899 <85224768+surajpatil4899@users.noreply.github.com>
2022-02-02 23:23:54 +0530

    Add files via upload

---
+++ Monk and his Father/Monk_and_his_father.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long p;
+        cin>>p;
+        cout<<__builtin_popcountll(p)<<endl;
+    }
+}
