bd39c86dc49f52981cb5ed9e4b1e2813507dfbb1
surajpatil4899 <85224768+surajpatil4899@users.noreply.github.com>
2022-02-09 23:42:26 +0530

    Add files via upload

---
+++ Chef and His Fruit Stand/Code.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+	int x,y;
+	cin>>x>>y;
+	int banana = x/2;
+      if(banana<y) cout<<banana<<endl;
+      else cout<<y<<endl;
+	}
+}
