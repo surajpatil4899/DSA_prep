d0766491ee091627446929bfbfaa0839807ac959
surajpatil4899 <85224768+surajpatil4899@users.noreply.github.com>
2022-02-10 00:33:38 +0530

    Add files via upload

---
+++ Delete Nodwe in a BST/Delete_Node_in_a_BST.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    TreeNode* findMin(TreeNode* root){
+            while(root->left != NULL) root = root->left;
+	         return root;
+    }
+    
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root == NULL) return root; 
+	else if(key < root->val) root->left = deleteNode(root->left,key);
+	else if (key > root->val) root->right = deleteNode(root->right,key);
+	// Wohoo... I found you, Get ready to be deleted	
+	else {
+		// Case 1:  No child
+		if(root->left == NULL && root->right == NULL) { 
+			delete root;
+			root = NULL;
+		}
+		//Case 2: One child 
+		else if(root->left == NULL) {
+			TreeNode *temp = root;
+			root = root->right;
+			delete temp;
+		}
+		else if(root->right == NULL) {
+			TreeNode *temp = root;
+			root = root->left;
+			delete temp;
+		}
+		// case 3: 2 children
+		else { 
+			TreeNode *temp = findMin(root->right);
+			root->val = temp->val;
+			root->right = deleteNode(root->right,temp->val);
+		}
+	}
+	return root;
+    }
+};
