3132bdd05b8ec32c2e7f87a44b5e55dd1beffc58
surajpatil4899 <85224768+surajpatil4899@users.noreply.github.com>
2022-02-28 22:01:48 +0530

    Add files via upload

---
+++ City and Flood/City_and_Flood.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e5+10;
+int parent[N];
+int size[N];
+
+void make(int v){
+   parent[v] = v;
+   size[v] = 1;
+}
+
+int find(int v){
+   if(v == parent[v]) return v;
+
+   return parent[v] = find(parent[v]);
+}
+
+void Union(int a,int b){
+   a = find(a);
+   b = find(b);
+   if(a != b){
+      if(size[a] < size[b])
+         swap(a,b);
+      parent[b] = a;
+      size[a] += size[b];
+   }
+
+}
+
+int main(){
+   int n,k;
+   cin>>n>>k;
+   for(int i=1;i<=n;i++){
+      make(i);
+   }
+   while(k--){
+      int u,v;
+      cin>>u>>v;
+      Union(u,v);
+   }
+   int count = 0;
+   for(int i=1;i<=n;i++){
+      if(find(i)==i) count++;
+   }
+   cout<<count<<endl;
+}
