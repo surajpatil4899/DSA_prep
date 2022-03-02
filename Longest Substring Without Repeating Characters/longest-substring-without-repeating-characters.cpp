a1ae38f355c8f3ec712805b01dbc40dbe391303d
surajpatil4899 <85224768+surajpatil4899@users.noreply.github.com>
2022-03-02 18:32:21 +0530

    Add files via upload

---
+++ Longest Substring Without Repeating Characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+      vector<int> map(256, -1);
+        int left=0,right=0,len=0;
+        while(right<s.size()){
+            if(map[s[right]] != -1) left = max(map[s[right]]+1, left);
+            map[s[right]] = right;
+            len = max(len, right-left+1);
+            right++;
+        }
+        
+        return len;
+    }
+};
