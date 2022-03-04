e011257bda52dcc563774aeeca39099ffd6959eb
surajpatil4899 <85224768+surajpatil4899@users.noreply.github.com>
2022-03-04 23:26:30 +0530

    Add files via upload

---
+++ Valid Anagram/Valid_Anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        unordered_map<char,int> m;
+        for(int i=0;i<s.size();i++){
+            m[s[i]]++;
+            m[t[i]]--;
+        }
+        for(auto it:m){
+            if(it.second) return false;
+        }
+        
+        return true;
+    }
+};
