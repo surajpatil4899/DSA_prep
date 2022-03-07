397330e0cb44da11eda652dc4ac6dae9eb25d9c5
surajpatil4899 <85224768+surajpatil4899@users.noreply.github.com>
2022-03-07 19:38:14 +0530

    Add files via upload

---
+++ Valid Sudoku/Valid_Sudoku.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+           vector<set<int>> row(9),col(9),block(9);
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.') continue;
+                int curr = board[i][j];
+                if(row[i].count(curr) || col[j].count(curr) || block[(i/3)*3 + j/3].count(curr))
+                    return false;
+                row[i].insert(curr);
+                col[j].insert(curr);
+                block[(i/3)*3 + j/3].insert(curr);
+            }
+        }
+        
+        return true;
+    }
+};
