b85f82493b9e002bdcfcce7ff0295c5202a9ff23
surajpatil4899 <85224768+surajpatil4899@users.noreply.github.com>
2022-03-11 13:13:30 +0530

    Add files via upload

---
+++ Search in Rotated Sorted Array/Search_In_Rotated_Sorted_Array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+       int lo = 0;
+        int hi = nums.size()-1;
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[lo]<=nums[mid]){
+                if(nums[lo]<=target && nums[mid]>=target){
+                    hi = mid-1;
+                }else {
+                    lo = mid + 1;
+                }
+            }else{
+                if(nums[mid]<=target && nums[hi]>=target){
+                    lo = mid + 1;
+                }else{
+                    hi = mid -1;
+                }
+            }
+        }
+        
+        return -1;
+    }
+};
