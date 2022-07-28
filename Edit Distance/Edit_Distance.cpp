b8a187bc4363bd4ae17c9cb641466a8e30aa2959
surajpatil4899 <85224768+surajpatil4899@users.noreply.github.com>
2022-07-28 20:02:31 +0530

    Add files via upload

--- Edit Distance/Edit_Distance.cpp
+++ Edit Distance/Edit_Distance.cpp
@@ -1,7 +1,10 @@
-class Solution {
-  public:
-  int dp[101][101];
-  int solve(string &s1,string &s2,int i,int j){
+#include <bits/stdc++.h>
+using namespace std;
+
+ 
+int dp[5001][5001];
+ 
+ int solve(string &s1,string &s2,int i,int j){
       if(i==0) return j;
       if(j==0) return i;
       
@@ -13,8 +16,17 @@ class Solution {
        return dp[i][j] = 1 + min(solve(s1,s2,i,j-1), min(solve(s1,s2,i-1,j),
        solve(s1,s2,i-1,j-1)));
   }
-    int editDistance(string s, string t) {
-        memset(dp, -1, sizeof(dp));
-        return solve(s,t,s.size(),t.size());
+ 
+int main(){
+
+    int t=1;
+    // cin>>t;
+    memset(dp, -1 , sizeof(dp));
+    
+    while(t--){
+    	string s1,s2;
+    	cin>>s1>>s2;
+       cout << solve(s1,s2,s1.size(),s2.size());     
     }
-};
+    return 0;
+}
